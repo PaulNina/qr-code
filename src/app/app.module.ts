398b765400e2c7dd8214ec4b63d2e6ad0d7f742f
Paul Nina <paul.ninat@gmail.com>
2023-05-10 08:55:45 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { QrGeneradorComponent } from './qr-generador/qr-generador.component';
+import {FormsModule,FormBuilder} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrGeneradorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    FormsModule
+  ],
+  exports: [
+    
+    QrGeneradorComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
